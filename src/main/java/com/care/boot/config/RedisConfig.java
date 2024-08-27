952de4621c39187f3b4f3e70c74bb157be1b991f
KKR <0088skg@naver.com>
2024-08-27 21:59:35 +0900

    REDIS

---
+++ src/main/java/com/care/boot/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.care.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("kkr-test-redis.j7jkni.ng.0001.apn2.cache.amazonaws.com", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        
+        
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        
+        return template;
+    }
+}
--- src/main/resources/META-INF/resources/WEB-INF/default/main.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/default/main.jsp
@@ -3,7 +3,6 @@
     
 <div align="center" class="main_div">
 	더할 나위 없다.<br>
-	TEST<br>
-	asdasd<br>
+	ABCDEF<br>
 	로그인 아이디 : ${sessionScope.id }<br>
 </div>
